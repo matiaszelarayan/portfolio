d525ac5eb49b8ad6d7ab7767992dc30029c3ff4c
Matías Zelarayán <matiaszelarayandev@gmail.com>
2025-12-22 20:37:49 -0300

    Configuración lista para GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from 'astro/config';
 
 
+
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
-  site: 'https://matiaszelarayan.github.io/portfolio/',
-  base: '/portfolio/',
+  site: 'https://matiaszelarayan.github.io',
+  base: '/portfolio',
+  
   // ...otros ajustes
 });
